fdab39ff63ceba225192a33ef4e78e888d8deafb
abod564 <abdelrahmanabod5664@gmail.com>
2025-09-15 18:14:38 +0300

    edit-year

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 function userScroll() {
-  const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector(".navbar");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('bg-dark');
-      navbar.classList.add('navbar-sticky');
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
     } else {
-      navbar.classList.remove('bg-dark');
-      navbar.classList.remove('navbar-sticky');
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
     }
   });
 }
 
-document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener("DOMContentLoaded", userScroll);
+
+const year = document.querySelector(".year");
+const curYear = new Date().getFullYear();
+year.textContent = curYear;
